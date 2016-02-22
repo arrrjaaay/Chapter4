9660dbb730500ad01d8931a21c0d8e8d8450bd36
RJ Santos <rjs4nt0s@gmail.com>
2016-02-22 11:06:09 -0500

    V1.0
    Finished "Sum"

---
+++ Sum/ConsoleApplication1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GetVowel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input; // Declare the variables.
+
+            Console.WriteLine("Enter a vowel.");
+            input = Console.ReadLine();
+
+            while (input == "A" || "a")
+            {
+                Console.WriteLine("Enter a vowel.");
+                input = Console.ReadLine();
+
+                // until the character entered is not a vowel
+                // it will repeat the loop
+            }
+        }
+    }
+}
---
+++ Sum/Sum/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            int num, total; // Declare the variables.
+
+            Console.WriteLine("Enter a number from 0 to 999.");
+            total = 0;
+            input = Console.ReadLine();
+            num = Convert.ToInt32(input); // Enter a number
+            
+            while(num < 999)
+            {
+                total += num;
+                Console.WriteLine("Enter a number from 0 to 999.");
+                input = Console.ReadLine();
+                num = Convert.ToInt32(input);
+                
+                // until the number that is entered is 999
+                // it will repeat the loop
+            }
+
+            Console.WriteLine("The total of the numbers you entered was {0}.", total);
+        }
+    }
+}
