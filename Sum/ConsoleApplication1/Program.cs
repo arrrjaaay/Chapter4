b400b9fce9b6f5026835ab55aa11fe704eb2a502
RJ Santos <rjs4nt0s@gmail.com>
2016-02-22 11:34:48 -0500

    V1.1
    Finished "GetVowel"

--- Sum/ConsoleApplication1/Program.cs
+++ Sum/ConsoleApplication1/Program.cs
@@ -10,18 +10,31 @@ namespace GetVowel
         static void Main(string[] args)
         {
             string input; // Declare the variables.
+            string a = "a";
+            string e = "e";
+            string i = "i";
+            string o = "o";
+            string u = "u";
+            string capa = "A";
+            string cape = "E";
+            string capi = "I";
+            string capo = "O";
+            string capu = "U";
 
             Console.WriteLine("Enter a vowel.");
             input = Console.ReadLine();
 
-            while (input == "A" || "a")
+            while (input == a || input == e || input == i || input == o || input == u ||
+                   input == capa || input == cape || input == capi || input == capo || input == capu)
             {
+                Console.WriteLine("OK.");
                 Console.WriteLine("Enter a vowel.");
                 input = Console.ReadLine();
 
                 // until the character entered is not a vowel
                 // it will repeat the loop
             }
+            Console.WriteLine("Error: '{0}' entered not a vowel.", input);
         }
     }
 }
