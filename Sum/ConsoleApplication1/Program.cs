dd14fdfb3a4758e98f1396c53c7538457fb90408
RJ Santos <rjs4nt0s@gmail.com>
2016-02-24 11:34:18 -0500

    V1.2
    "finished" Multiplication Table

--- Sum/ConsoleApplication1/Program.cs
+++ Sum/ConsoleApplication1/Program.cs
@@ -10,27 +10,16 @@ namespace GetVowel
         static void Main(string[] args)
         {
             string input; // Declare the variables.
-            string a = "a";
-            string e = "e";
-            string i = "i";
-            string o = "o";
-            string u = "u";
-            string capa = "A";
-            string cape = "E";
-            string capi = "I";
-            string capo = "O";
-            string capu = "U";
 
             Console.WriteLine("Enter a vowel.");
             input = Console.ReadLine();
 
-            while (input == a || input == e || input == i || input == o || input == u ||
-                   input == capa || input == cape || input == capi || input == capo || input == capu)
+            while (input == "a" || input == "e" || input == "i" || input == "o" || input == "u"
+                || input == "A" || input == "E" || input == "I" || input == "O" || input == "U")
             {
                 Console.WriteLine("OK.");
                 Console.WriteLine("Enter a vowel.");
                 input = Console.ReadLine();
-
                 // until the character entered is not a vowel
                 // it will repeat the loop
             }
